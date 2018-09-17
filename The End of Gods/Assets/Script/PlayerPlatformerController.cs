33e283129036e28cf4382e12ddfa84c6850b53e9
Antoine <awdawy>
2018-09-17 16:42:56 -0400

    Mouvement saut + camera a moitier

---
+++ The End of Gods/Assets/Script/PlayerPlatformerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPlatformerController : PhysicsObject
+{
+
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 12;
+
+    private Animator animator;
+    private SpriteRenderer sprite;
+
+    bool isGauche;
+
+    // Use this for initialization
+    void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+
+        move.x = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = jumpTakeOffSpeed;
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            if (velocity.y > 0)
+            {
+                velocity.y = velocity.y * 0.5f;
+            }
+        }
+
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            if (isGauche == false)
+            sprite.flipX = !sprite.flipX;
+
+            isGauche = true;
+        }
+        else if (Input.GetAxis("Horizontal") > 0)
+        {
+            if (isGauche == true)
+                sprite.flipX = !sprite.flipX;
+
+            isGauche = false;
+        }
+
+        animator.SetBool("grounded", grounded);
+        animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+
+        targetVelocity = move * maxSpeed;
+    }
+}
