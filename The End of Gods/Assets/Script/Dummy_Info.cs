1b37b8df65d00e8c1c40f080ba30a3fd08896a7a
Antoine <efqwef>
2018-10-05 19:29:28 -0400

    Faite dequoi un peu

---
+++ The End of Gods/Assets/Script/Dummy_Info.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dummy_Info : MonoBehaviour {
+
+    public int startingHealth = 999999999;
+    public float defence = 0.0f;
+    public int currentHealth;
+    public Image damageImage;
+    public float flashSpeed = 5f;
+    public Color flashColour = new Color(1f, 0f, 0f, 1f);
+
+    // Use this for initialization
+    bool damage;
+
+    void Awake()
+    {
+        currentHealth = startingHealth;
+    }
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (damage)
+        {
+            damageImage.color = flashColour;
+        }
+        else
+        {
+            damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        damage = false;
+	}
+
+    public void DealDamage(int amount)
+    {
+        damage = true;
+        currentHealth -= amount;
+    }
+
+
+
+
+}
---
+++ The End of Gods/Assets/Script/Player_Info.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Info : MonoBehaviour {
+    public int startingHealth = 100;
+    public int currentHealth;
+
+    PlayerPlatformerController PlayerMovement;
+
+    void Awake()
+    {
+        PlayerMovement = GetComponent<PlayerPlatformerController>();
+        currentHealth = startingHealth;
+    }
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
